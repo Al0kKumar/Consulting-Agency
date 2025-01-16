3e794f72b9208468f5028fb6d9cc49e6e5e6c7a7
Alok <mishraalok189381@gmail.com>
2025-01-16 17:49:33 +0530

    fixed firebase

--- src/firebaseBaseConfig.js
+++ src/firebaseBaseConfig.js
@@ -5,4 +5,5 @@ const firebaseConfig = {
     storageBucket: "easy-consulting-react.appspot.com",
     messagingSenderId: "471032014480",
     appId: "1:471032014480:web:cbd7dc3d102ddd71d55a15"
-  };export default firebaseConfig;
+  };
+export default firebaseConfig;
